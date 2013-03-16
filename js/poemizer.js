9b3490b20493e624ed8c4346e57b44cd96e2f552
Arvid Jakobsson <arvid.jakobsson@gmail.com>
2013-03-16 17:09:23 +0100

    Add more poems

--- js/poemizer.js
+++ js/poemizer.js
@@ -17,8 +17,41 @@ jQuery(function ($) {
 			"Nothing beside remains. Round the decay\n" +
 			"Of that colossal wreck, boundless and bare\n" +
 			"The lone and level sands stretch far away.\n",
-		"Demain, dès l'aube - Viktor Hugo": "",
-		"RIMBAUD : Ma Bohème (Fantaisie oct. 1870)": ""
+
+		"Ma Bohème - Rimbaud":
+		"Je m'en allais, les poings dans mes poches crevées ;\n" +
+			"Mon paletot aussi devenait idéal :\n" +
+			"J'allais sous le ciel, Muse ! et j'étais ton féal ;\n" +
+			"Oh ! là là ! que d'amours splendides j'ai rêvées!\n" +
+			"\n" +
+			"Mon unique culotte avait un large trou.\n" +
+			"- Petit-Poucet rêveur, j'égrenais dans ma course\n" +
+			"Des rimes. Mon auberge était à la Grande-Ourse.\n" +
+			"- Mes étoiles au ciel avaient un doux frou-frou\n" +
+			"\n" +
+			"Et je les écoutais, assis au bord des routes,\n" +
+			"Ces bons soirs de septembre où je sentais des gouttes\n" +
+			"De rosée à mon front, comme un vin de vigueur ;\n" +
+			"\n" +
+			"Où, rimant au milieu des ombres fantastiques,\n" +
+			"Comme des lyres, je tirais les élastiques\n" +
+			"De mes souliers blessés, un pied près de mon cœur !\n",
+
+		"Demain, dès l'aube - Viktor Hugo":
+		"Demain, dès l'aube, à l'heure où blanchit la campagne,\n" +
+			"Je partirai. Vois-tu, je sais que tu m'attends.\n" +
+			"J'irai par la forêt, j'irai par la montagne.\n" +
+			"Je ne puis demeurer loin de toi plus longtemps.\n" +
+			"\n" +
+			"Je marcherai les yeux fixés sur mes pensées,\n" +
+			"Sans rien voir au dehors, sans entendre aucun bruit,\n" +
+			"Seul, inconnu, le dos courbé, les mains croisées,\n" +
+			"Triste, et le jour pour moi sera comme la nuit.\n" +
+			"\n" +
+			"Je ne regarderai ni l'or du soir qui tombe,\n" +
+			"Ni les voiles au loin descendant vers Harfleur,\n" +
+			"Et quand j'arriverai, je mettrai sur ta tombe\n" +
+			"Un bouquet de houx vert et de bruyère en fleur.\n"
 	};
 	var step = 0, poem = '', poem_lines, chunk_size, chunk;
 
